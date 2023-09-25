bbc248a5d37309cbd85aea559dff90cb40839718
Mark Davis <Markd@trifoia.com>
2023-09-24 22:19:26 -0700

    feat: Update Detail, Edit, and Index components
    - Updated the namespace in Detail.razor and Edit.razor files
    - Changed the typography of MudText in Detail.razor
    - Changed the color of MudButton in Detail.razor
    - Added a new method OnParametersSetAsync() in Detail.razor
    - Removed unused code from Edit.razor
    - Added form validation and save functionality to Edit.razor
    - Added a new method Back() in Edit.razor
    - Updated the layout and functionality of Index.razor

--- OE.Theme.LHB/Client/ThemeInfo.cs
+++ OE.Theme.LHB/Client/ThemeInfo.cs
@@ -16,6 +16,10 @@ namespace OE.Theme.LHB
             Resources = new List<Resource>()
             {
                 new Resource { ResourceType = ResourceType.Script, Url = "_content/MudBlazor_oqtane/MudBlazor.min.js",Level=ResourceLevel.Site },
+                //new Resource { ResourceType = ResourceType.Stylesheet, Url = "_content/MudBlazor_oqtane/MudBlazor.min.css",Level=ResourceLevel.Site },
+                //new Resource { ResourceType = ResourceType.Stylesheet, Url = "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap",Level=ResourceLevel.Site },
+                //new Resource { ResourceType = ResourceType.Stylesheet, Url = "Themes/OE.Theme.LHB/Theme.css",Level=ResourceLevel.Site },
+                // the above doens't work, so we'll just add the css to the ThemeLJB.razor file
             }
         };
 
