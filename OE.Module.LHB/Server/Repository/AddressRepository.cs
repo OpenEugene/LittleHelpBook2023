853be5526a06c2b71d0c339b4262a62a215b80a3
Geoff Caylor <rolyac@flisher.net>
2023-10-06 10:20:14 -0700

    Small change to Address repo

--- OE.Module.LHB/Server/Repository/AddressRepository.cs
+++ OE.Module.LHB/Server/Repository/AddressRepository.cs
@@ -2,27 +2,22 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Collections.Generic;
 using Oqtane.Modules;
-
 using M = OE.Module.LHB.Shared.Models;
 
-namespace OE.Module.LHB.Repository
-{
-    public class AddressRepository :  ITransientService
-    {
+namespace OE.Module.LHB.Repository {
+    public class AddressRepository : ITransientService {
         private readonly LHBContext _db;
 
-        public AddressRepository(LHBContext context)
-        {
+        public AddressRepository(LHBContext context) {
             _db = context;
         }
 
-        public M.Address GetAddressByProviderId(int providerId) {
+        public M.Address GetAddressByProviderId(int providerId, bool tracking = false) {
             var providerAddress = _db.ProviderAddress.FirstOrDefault(pa => pa.ProviderId == providerId);
-            return providerAddress != null ? _db.Address.FirstOrDefault(a => a.AddressId == providerAddress.AddressId) : null;
+            return providerAddress != null ? GetAddress(providerAddress.AddressId, tracking) : null;
         }
 
-        public M.Address GetAddressByAddressId(int addressId)
-        {
+        public M.Address GetAddressByAddressId(int addressId) {
             return GetAddress(addressId, true);
         }
 
@@ -30,28 +25,24 @@ namespace OE.Module.LHB.Repository
             return tracking ? _db.Address.Find(addressId) : _db.Address.AsNoTracking().FirstOrDefault(item => item.AddressId == addressId);
         }
 
-        public M.Address AddAddress(M.Address item)
-        {
+        public M.Address AddAddress(M.Address item) {
             _db.Address.Add(item);
             _db.SaveChanges();
             return item;
         }
 
-        public M.Address UpdateAddress(M.Address addressId)
-        {
+        public M.Address UpdateAddress(M.Address addressId) {
             _db.Entry(addressId).State = EntityState.Modified;
             _db.SaveChanges();
             return addressId;
         }
 
-        public void DeleteAddress(int addressId)
-        {
+        public void DeleteAddress(int addressId) {
             var item = _db.Address.Find(addressId);
 
             if (item == null) return;
             _db.Address.Remove(item);
             _db.SaveChanges();
-
         }
     }
 }
